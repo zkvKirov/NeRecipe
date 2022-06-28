d1b1a630585ace0a2d0e96c70d0261140298bed7
zkvKirov <zkkirov@yandex.ru>
2022-06-28 13:02:54 +0300

    fix some bugs 2

--- app/src/main/java/ru/netology/nerecipe/data/RecipeCreateResult.kt
+++ app/src/main/java/ru/netology/nerecipe/data/RecipeCreateResult.kt
@@ -5,9 +5,9 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class RecipeCreateResult (
-    val newTitle: String,
-    val newAuthor: String,
-    val newCategory: String,
-    val newStep1: String,
-    val newStep2: String
+    val newTitle: String?,
+    val newAuthor: String?,
+    val newCategory: String?,
+    val newStep1: String?,
+    val newStep2: String?
 ) : Parcelable
