e0032a2e82a49d15664b55786030b6afae760e29
zkvKirov <100080290+zkvKirov@users.noreply.github.com>
2022-07-25 23:52:46 +0300

    добавлено создание шагов в отдельном фрагменте

--- app/src/test/java/ru/netology/nerecipe/viewModel/RecipeViewModelTest.kt
+++ app/src/test/java/ru/netology/nerecipe/viewModel/RecipeViewModelTest.kt
@@ -9,16 +9,16 @@ import ru.netology.nerecipe.data.RecipeCreateResult
 
 internal class RecipeViewModelTest {
 
-    @Ignore
-    @Test
-    fun onSaveButtonClicked_newRecipe() {
-        //arrange
-        val viewModel = RecipeViewModel(application = Application())
-        val expectedRecipeCard = RecipeCard(id = 0, title = "салат", author = "Я", category = "Азия", isFavorite = false)
-        //act
-        val actualRecipeCard = viewModel.onSaveButtonClicked(RecipeCreateResult(newTitle = "салат", newAuthor = "Я", newCategory = "Азия"))
-        //assert
-        assertEquals(expectedRecipeCard, actualRecipeCard)
-    }
+//    @Ignore
+//    @Test
+//    fun onSaveButtonClicked_newRecipe() {
+//        //arrange
+//        val viewModel = RecipeViewModel(application = Application())
+//        val expectedRecipeCard = RecipeCard(id = 0, title = "салат", author = "Я", category = "Азия", isFavorite = false)
+//        //act
+//        val actualRecipeCard = viewModel.onSaveButtonClicked(RecipeCreateResult(newTitle = "салат", newAuthor = "Я", newCategory = "Азия"))
+//        //assert
+//        assertEquals(expectedRecipeCard, actualRecipeCard)
+//    }
 }
 
